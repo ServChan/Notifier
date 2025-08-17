337bc57fa229d6f8bfae15d46ba1b444e180c030
ServChan <10271750+ServChan@users.noreply.github.com>
2025-08-17 13:19:31 +0300

    Дежурное обновление

--- config.py
+++ config.py
@@ -1,13 +1,4 @@
-# Данные можно получить на my.telegram.org
-# Data can be obtained at my.telegram.org
-
-# Что угодно формата "session-12381293812"
-# Anything like the "session-12381293812" format
-session_name = "session-123456"
-# Data from my.telegram.org
-api_id = 123456
-api_hash = "qwertyuiop_123456"
-
-# Символ, определяющий что это команда. Можно оставить один, можно сделать несколько в формате ["!", "/"]
-# A character identifying that this is a command. You can leave one, you can make several in the format ["!", "/"]
-command_prefix = [">"]
+session_name="notifier_session"
+api_id=123456
+api_hash="qwertyuiop_123456"
+command_prefix=[">"]
